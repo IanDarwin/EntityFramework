b5092144a1dbb604a93443ca761ff136bf229cf3
IanDarwin <>
2013-03-29 23:21:00 +0000

    Build the DAO layer, using EntityHome/EntityList

---
+++ src/main/java/com/darwinsys/entityframe/EntityHome.java
@@ -0,0 +1,42 @@
+package dao;
+
+import javax.persistence.EntityManager;
+
+/**
+ * Patterned loosely after the Seam2 Entity Framework; contains
+ * methods to manipulate one entity.
+ * @author Ian Darwin
+ * @param <T> The type of the JPA Entity we want to manipulate.
+ */
+public abstract class EntityHome<T extends Object> {
+
+	abstract EntityManager getEntityManager();
+	protected Class<T> entityClass;
+	
+	@SuppressWarnings("unchecked")
+	protected EntityHome() {
+		entityClass = (Class<T>) EntityQuery.getEntityClass(this);
+		System.out.println("Entity class = " + entityClass.getName());
+	}
+
+	/** The C of CRUD - create a new T in the database */
+	public void persist(T entity) {
+		getEntityManager().persist(entity);
+	}
+
+	/** The R of CRUD - Download a T by primary key */
+	public T find(long id) {		
+		return getEntityManager().find(entityClass, id);
+	}
+	
+	/** The U of CRUD - update an Entity */
+	public void update(T entity) {
+		// Nothing to do here - if the Entity is persistent, changes to
+		// it will be persisted by the EntityManager automagically.
+	}
+	
+	/** The D of CRUD - delete an Entity. Use with care! */
+	public void delete(T entity) {
+		getEntityManager().remove(entity);
+	}
+}
---
+++ src/main/java/com/darwinsys/entityframe/EntityList.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Patterned loosely after the Seam2 Entity Framework: methods for getting a list of Entities.
+ * @author Ian Darwin
+ *
+ * @param <T> The type of the JPA entity that you want to list.
+ */
+public abstract class EntityList<T extends Object> {
+
+	abstract EntityManager getEntityManager();
+	
+	protected Class<T> entityClass;
+	
+	@SuppressWarnings("unchecked")
+	protected EntityList() {
+		entityClass = (Class<T>) EntityQuery.getEntityClass(this);
+		System.out.println("Entity class = " + entityClass.getName());
+	}
+	
+	public List<T> getResultList() {
+		System.out.printf("EntityList<%s>.getResultList()", entityClass.getName());
+		return getEntityManager().createQuery("from " + entityClass.getName()).getResultList();
+	}
+	
+	public int getResultCount() {
+		return (int) getEntityManager().createQuery("select count(*) from " + entityClass.getName()).getSingleResult();
+	}
+}
