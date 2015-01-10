38bfc24c0f2622ea944a55abd4e330ac3d6dc38c
Ian Darwin <ian@darwinsys.com>
2015-01-10 11:08:58 -0500

    More methods needed for Seam2 compat, hope they are correct.

--- src/main/java/com/darwinsys/entityframe/EntityHome.java
+++ src/main/java/com/darwinsys/entityframe/EntityHome.java
@@ -25,6 +25,8 @@ public abstract class EntityHome<T extends Object> {
 
 	public abstract EntityManager getEntityManager();
 	protected Class<T> entityClass;
+	protected T instance;
+	protected Object pk;	// XXX should be a type parameter
 	
 	@SuppressWarnings("unchecked")
 	protected EntityHome() {
@@ -32,6 +34,34 @@ public abstract class EntityHome<T extends Object> {
 		System.out.println("Entity class = " + entityClass.getName());
 	}
 
+	public void create() {
+		try {
+			this.instance = entityClass.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			throw new RuntimeException("Failed to create instance: " + e, e);
+		}
+	}
+	
+	protected T createInstance() {
+		create();
+		return instance;
+	}
+
+	protected void setId(Object pk) {
+		this.pk = pk;
+	}
+	protected Object getId() {
+		return pk;
+	}
+
+	// This shalle be the only path that changes the 'instance' variable...
+    public void setInstance(T t) {
+		this.instance = t;
+	}
+	public T getInstance() {
+		return instance;
+	}
+	
 	/** The C of CRUD - create a new T in the database */
 	public void persist(T entity) {
 		getEntityManager().persist(entity);
